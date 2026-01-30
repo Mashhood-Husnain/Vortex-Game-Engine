5a5ffdd70e942a4583b55da5b622fa4043e78448
Mashhood Husnain <113771764+babaranjitsing@users.noreply.github.com>
2026-01-30 23:17:19 +0500

    added fullscreen toggle function, updated TODO

--- src/opengl_window.cpp
+++ src/opengl_window.cpp
@@ -1,5 +1,39 @@
 #include "opengl_window.hpp"
 
+GLFWmonitor* OpenGLWindow::get_current_monitor(GLFWwindow* window)
+{
+    int nmonitors;
+    int wx, wy, ww, wh;
+    int mx, my, mw, mh;
+    int overlap, bestoverlap = 0;
+    GLFWmonitor* bestmonitor = nullptr;
+    GLFWmonitor**monitors;
+    const GLFWvidmode* mode;
+
+    glfwGetWindowPos(window, &wx, &wy);
+    glfwGetWindowSize(window, &ww, &wh);
+    monitors = glfwGetMonitors(&nmonitors);
+
+    for (int i = 0; i < nmonitors; i++)
+    {
+        mode = glfwGetVideoMode(monitors[i]);
+        glfwGetMonitorPos(monitors[i], &mx, &my);
+        mw = mode->width;
+        mh = mode->height;
+
+        overlap = 
+            std::max(0, std::min(wx+ww, mx+mw) - std::max(wx, mx)) *
+            std::max(0, std::min(wy+wh, my+mh) - std::max(wy, my));
+        
+        if (bestoverlap < overlap)
+        {
+            bestoverlap = overlap;
+            bestmonitor = monitors[i];
+        }
+    }
+    return bestmonitor;
+}
+
 void OpenGLWindow::framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     auto* app = static_cast<OpenGLWindow*>(glfwGetWindowUserPointer(window));
@@ -9,6 +43,27 @@ void OpenGLWindow::framebuffer_size_callback(GLFWwindow* window, int width, int 
         glViewport(0, 0, width, height);
         app->default_window_width = width;
         app->default_window_height = height;
+    }
+}
+
+void OpenGLWindow::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    auto* app = static_cast<OpenGLWindow*>(glfwGetWindowUserPointer(window));
+
+    if (!app) return;
+
+    if (action == GLFW_PRESS)
+    {
+        switch (key)
+        {
+            case GLFW_KEY_ESCAPE:
+                glfwSetWindowShouldClose(window, true);
+                break;
+            case GLFW_KEY_F:
+                app->is_fullscreen = !app->is_fullscreen;
+                app->set_fullscreen();
+                break;
+        }
     }
 }
 
@@ -40,6 +95,7 @@ OpenGLWindow::OpenGLWindow(std::string window_name, int width, int height)
     glfwMakeContextCurrent(window);
     glfwSetWindowUserPointer(window, this);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, key_callback);
 
     // Initialize GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -47,6 +103,10 @@ OpenGLWindow::OpenGLWindow(std::string window_name, int width, int height)
         std::cerr << "Failed to initialize GLAD" << std::endl;
         exit(EXIT_FAILURE);
     }
+
+    stored_window_width = width;
+    stored_window_height = height;
+    glfwGetWindowPos(window, &stored_window_x_pos, &stored_window_y_pos);
 }
 
 OpenGLWindow::~OpenGLWindow()
@@ -55,21 +115,29 @@ OpenGLWindow::~OpenGLWindow()
     glfwTerminate();
 }
 
-void OpenGLWindow::check_input()
+void OpenGLWindow::set_fullscreen()
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (is_fullscreen)
     {
-        glfwSetWindowShouldClose(window, true);
+        glfwGetWindowPos(window, &stored_window_x_pos, &stored_window_y_pos);
+        glfwGetWindowSize(window, &stored_window_width, &stored_window_height);
+
+        GLFWmonitor* monitor = get_current_monitor(window);
+        const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+        glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+        glfwFocusWindow(window);
+        return;
     }
+
+    glfwSetWindowMonitor(window, nullptr, stored_window_x_pos, stored_window_y_pos, stored_window_width, stored_window_height, 0);
+    glfwFocusWindow(window);
 }
 
 void OpenGLWindow::run()
 {
     while(!glfwWindowShouldClose(window))
     {
-        // Check for any user inputs
-        check_input();
-
         // rendering
         glClearColor(bg_color.r, bg_color.g, bg_color.b, bg_color.a);
         glClear(GL_COLOR_BUFFER_BIT);
--- src/opengl_window.hpp
+++ src/opengl_window.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -8,13 +9,20 @@
 class OpenGLWindow
 {
     GLFWwindow *window = nullptr;
-    int default_window_width;
-    int default_window_height;
+    int default_window_width = 800;
+    int default_window_height = 600;
     std::string window_name;
-    glm::vec4 bg_color = glm::vec4(0.2f, 0.3f, 0.3f, 1.0f);
+    glm::vec4 bg_color = glm::vec4(0.1f, 0.1f, 0.1f, 1.0f);
+    bool is_fullscreen = false;
+    int stored_window_x_pos;
+    int stored_window_y_pos;
+    int stored_window_width;
+    int stored_window_height;
 
+    GLFWmonitor* get_current_monitor(GLFWwindow* window);
     static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-    void check_input();
+    static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+    void set_fullscreen();
 public:
     OpenGLWindow(std::string window_name, int width=800, int height=600);
     ~OpenGLWindow();
